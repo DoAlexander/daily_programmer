4dc6b5603589b1dd5ed935e3ac9097c74153a7a8
Doug Alexander <doalexander@csumb.edu>
2017-03-20 16:57:25 -0700

    Int to Numeral working

--- 306.py
+++ 306.py
@@ -15,3 +15,22 @@
 # See OEIS sequence A105416 for more information.
 # http://oeis.org/A105416
 
+import collections
+
+# Dict for numeral to int values | covers all possible combinations of letters | does not handle large number syntax for roman numerals
+d_numerals = {'M':1000,'CM':900,'D':500,'CD':400,'C':100,'XC':100,'L':50,'XL':40,'X':10,'IX':9,'V':5,'IV':4,'I':1}
+s_numerals = collections.OrderedDict(sorted(d_numerals.items(), key=lambda x: x[1], reverse=True))
+
+# Converts integers to numerals
+def int_to_numeral(arg):
+	num = arg
+	out = ''
+	while(num > 0):
+		for x,y in enumerate(s_numerals):
+			if num - s_numerals[y] >= 0:
+				num -= s_numerals[y]
+				out += y
+				break
+	return out
+
+print(int_to_numeral(2017))
